b2cdd72851354d3ac432b8f78c057f76a2d44b58
0xean <0xean.eth@gmail.com>
2022-01-05 14:31:10 -0700

    updated deploy script for mainnet deploy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,15 +1,9 @@
 from pathlib import Path
 
-from brownie import AffiliateToken, Contract, accounts, config, network, project, web3
+from brownie import ShapeShiftDAORouter, accounts, network, web3
 from eth_utils import is_checksum_address
 
 import click
-
-API_VERSION = config["dependencies"][0].split("@")[-1]
-Vault = project.load(
-    Path.home() / ".brownie" / "packages" / config["dependencies"][0]
-).Vault
-
 
 def get_address(msg: str, default: str = None) -> str:
 
@@ -35,35 +29,25 @@ def main():
     print(f"You are using the '{network.show_active()}' network")
     dev = accounts.load(click.prompt("Account", type=click.Choice(accounts.load())))
     print(f"You are using: 'dev' [{dev.address}]")
-    token = Contract.from_explorer(get_address("Underlying token"))
     registry = get_address("Registry address,", "v2.registry.ychad.eth")
-
-    affiliateTokenName = click.prompt("Enter the token name for your affiliate token")
-    affiliateTokenSymbol = click.prompt(
-        "Enter the token symbol for your affiliate token"
-    )
+    
 
     print(
         f"""
-    Affiliate Token Parameters
-        name: '{affiliateTokenName}'
-      symbol: '{affiliateTokenSymbol}'
-       token: {token.address}
+    ShapeShiftDAORouter Token Parameters
+        registry: '{registry}'
     """
     )
 
     publish_source = click.confirm("Verify source on etherscan?")
 
-    if input("Deploy Affiliate Token? y/[N]: ").lower() != "y":
+    if input("Deploy ShapeShiftDAORouter? y/[N]: ").lower() != "y":
         return
 
-    affiliatetoken = AffiliateToken.deploy(
-        token,
+    shapeShiftDAORouter = ShapeShiftDAORouter.deploy(
         registry,
-        affiliateTokenName,
-        affiliateTokenSymbol,
         {"from": dev},
         publish_source=publish_source,
     )
 
-    print(f"Deployed {affiliatetoken}")
+    print(f"Deployed {shapeShiftDAORouter}")
